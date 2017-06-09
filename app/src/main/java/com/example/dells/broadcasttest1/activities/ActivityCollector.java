6c9b927da765245c91191281d951820bd17e457d
DozenPiggy <dozenpiggy@163.com>
2017-06-09 16:56:36 +0800

    test

--- app/src/main/java/com/example/dells/broadcasttest1/activities/ActivityCollector.java
+++ app/src/main/java/com/example/dells/broadcasttest1/activities/ActivityCollector.java
@@ -24,6 +24,7 @@ public class ActivityCollector {
         for(Activity activity : activities){
             if(!activity.isFinishing()){
                 activity.finish();
+                //test
             }
         }
     }
